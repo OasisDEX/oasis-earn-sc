4bb089c1ff6938d0f6762f3e2b2b5979e2b12194
James Tuckett <james@tuckett.me>
2022-06-06 13:30:37 +0100

    chore: clean up abi folder, remove duplicate helpers and common constants in test dir

--- test/common/http-apis.ts
+++ test/common/http-apis.ts
@@ -1,46 +1,52 @@
-import { ethers } from 'hardhat'
-import fetch from 'node-fetch'
-import BigNumber from 'bignumber.js'
-import MAINNET_ADDRESSES from '../../addresses/mainnet.json'
-import { OneInchBaseResponse, OneInchSwapResponse } from './common.types'
+import { ethers } from "hardhat";
+import fetch from "node-fetch";
+import BigNumber from "bignumber.js";
+import MAINNET_ADDRESSES from "../../addresses/mainnet.json";
+import { OneInchBaseResponse, OneInchSwapResponse } from "./common.types";
 
 export async function getMarketPrice(
   from: string,
   to: string,
   fromPrecision = 18,
-  toPrecision = 18,
+  toPrecision = 18
 ) {
-  const amount = ethers.utils.parseUnits('0.1', fromPrecision)
-  const url = `https://api.1inch.exchange/v4.0/1/quote?fromTokenAddress=${from}&toTokenAddress=${to}&amount=${amount}&protocols=UNISWAP_V3`
+  const amount = ethers.utils.parseUnits("0.1", fromPrecision);
+  const url = `https://api.1inch.exchange/v4.0/1/quote?fromTokenAddress=${from}&toTokenAddress=${to}&amount=${amount}&protocols=UNISWAP_V3`;
 
-  const response = await fetch(url)
+  const response = await fetch(url);
 
   if (!response.ok) {
-    throw new Error(`Error performing 1inch quote request ${url}: ${await response.text()}`)
+    throw new Error(
+      `Error performing 1inch quote request ${url}: ${await response.text()}`
+    );
   }
 
-  const result = (await response.json()) as OneInchBaseResponse
+  const result = (await response.json()) as OneInchBaseResponse;
 
   const fromTokenAmount = new BigNumber(
-    ethers.utils.formatUnits(result.fromTokenAmount, fromPrecision),
-  )
-  const toTokenAmount = new BigNumber(ethers.utils.formatUnits(result.toTokenAmount, toPrecision))
+    ethers.utils.formatUnits(result.fromTokenAmount, fromPrecision)
+  );
+  const toTokenAmount = new BigNumber(
+    ethers.utils.formatUnits(result.toTokenAmount, toPrecision)
+  );
 
-  return toTokenAmount.div(fromTokenAmount)
+  return toTokenAmount.div(fromTokenAmount);
 }
 
 export async function getCurrentBlockNumber() {
-  const timestamp = Math.floor(Date.now() / 1000)
-  const url = `https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp=${timestamp}&closest=before&apikey=YAJI4NVD8QTQ9JVWG2NKN3FFUK6IZTMV5S` // TODO: remove api key
+  const timestamp = Math.floor(Date.now() / 1000);
+  const url = `https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp=${timestamp}&closest=before&apikey=YAJI4NVD8QTQ9JVWG2NKN3FFUK6IZTMV5S`; // TODO: remove api key
 
-  const response = await fetch(url)
+  const response = await fetch(url);
 
   if (!response.ok) {
-    throw new Error(`Could not fetch current block from etherscan: ${await response.text()}`)
+    throw new Error(
+      `Could not fetch current block from etherscan: ${await response.text()}`
+    );
   }
 
-  const body = (await response.json()) as { result: string }
-  return parseInt(body.result)
+  const body = (await response.json()) as { result: string };
+  return parseInt(body.result);
 }
 
 function formatOneInchSwapUrl(
@@ -49,20 +55,24 @@ function formatOneInchSwapUrl(
   amount: string,
   slippage: string,
   recepient: string,
-  protocols: string[] = [],
+  protocols: string[] = []
 ) {
-  const protocolsParam = !protocols?.length ? '' : `&protocols=${protocols.join(',')}`
-  return `https://oasis.api.enterprise.1inch.exchange/v4.0/1/swap?fromTokenAddress=${fromToken}&toTokenAddress=${toToken}&amount=${amount}&fromAddress=${recepient}&slippage=${slippage}${protocolsParam}&disableEstimate=true&allowPartialFill=false`
+  const protocolsParam = !protocols?.length
+    ? ""
+    : `&protocols=${protocols.join(",")}`;
+  return `https://oasis.api.enterprise.1inch.exchange/v4.0/1/swap?fromTokenAddress=${fromToken}&toTokenAddress=${toToken}&amount=${amount}&fromAddress=${recepient}&slippage=${slippage}${protocolsParam}&disableEstimate=true&allowPartialFill=false`;
 }
 
 async function exchangeTokens(url: string): Promise<OneInchSwapResponse> {
-  const response = await fetch(url)
+  const response = await fetch(url);
 
   if (!response.ok) {
-    throw new Error(`Error performing 1inch swap request ${url}: ${await response.text()}`)
+    throw new Error(
+      `Error performing 1inch swap request ${url}: ${await response.text()}`
+    );
   }
 
-  return response.json()
+  return response.json();
 }
 
 export async function exchangeFromDAI(
@@ -70,7 +80,7 @@ export async function exchangeFromDAI(
   amount: string,
   slippage: string,
   recepient: string,
-  protocols: string[] = [],
+  protocols: string[] = []
 ): Promise<OneInchSwapResponse> {
   const url = formatOneInchSwapUrl(
     MAINNET_ADDRESSES.MCD_DAI,
@@ -78,10 +88,10 @@ export async function exchangeFromDAI(
     amount,
     slippage,
     recepient,
-    protocols,
-  )
+    protocols
+  );
 
-  return exchangeTokens(url)
+  return exchangeTokens(url);
 }
 
 export async function exchangeToDAI(
@@ -89,7 +99,7 @@ export async function exchangeToDAI(
   amount: string,
   recepient: string,
   slippage: string,
-  protocols: string[] = [],
+  protocols: string[] = []
 ): Promise<OneInchSwapResponse> {
   const url = formatOneInchSwapUrl(
     fromTokenAddress,
@@ -97,8 +107,8 @@ export async function exchangeToDAI(
     amount,
     slippage,
     recepient,
-    protocols,
-  )
+    protocols
+  );
 
-  return exchangeTokens(url)
+  return exchangeTokens(url);
 }
