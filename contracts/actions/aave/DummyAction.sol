f5ef395d29eba9cd230cf55ebeedf2c0b7940b31
James Tuckett <james@tuckett.me>
2022-06-07 13:11:26 +0100

    chore: prettify codebase

--- contracts/actions/aave/DummyAction.sol
+++ contracts/actions/aave/DummyAction.sol
@@ -5,14 +5,14 @@ import "../../core/ServiceRegistry.sol";
 import "../../core/OperationStorage.sol";
 
 contract DummyAction is IAction {
-    // TODO: Pass the service registry in here
-    constructor(address _registry) IAction(_registry) {}
+  // TODO: Pass the service registry in here
+  constructor(address _registry) IAction(_registry) {}
 
-    function execute(bytes calldata data, uint8[] memory) external payable override {
-        OperationStorage txStorage = OperationStorage(
-            registry.getRegisteredService("OPERATION_STORAGE")
-        );
-        console.log("IN THE DUMMY ACTION");
-        console.log(txStorage.len());
-    }
+  function execute(bytes calldata data, uint8[] memory) external payable override {
+    OperationStorage txStorage = OperationStorage(
+      registry.getRegisteredService("OPERATION_STORAGE")
+    );
+    console.log("IN THE DUMMY ACTION");
+    console.log(txStorage.len());
+  }
 }
--- contracts/actions/maker/OpenVault.sol
+++ contracts/actions/maker/OpenVault.sol
@@ -8,26 +8,22 @@ import "../../core/ServiceRegistry.sol";
 import "../../interfaces/maker/IJoin.sol";
 import "../../interfaces/maker/IManager.sol";
 
-import {OpenVaultData} from "../../core/Types.sol";
+import { OpenVaultData } from "../../core/Types.sol";
 
 contract OpenVault is IAction {
-    constructor(address _registry) IAction(_registry) {}
+  constructor(address _registry) IAction(_registry) {}
 
-    function execute(bytes calldata data, uint8[] memory)
-        external
-        payable
-        override
-    {
-        OpenVaultData memory openVaultData = abi.decode(data, (OpenVaultData));
+  function execute(bytes calldata data, uint8[] memory) external payable override {
+    OpenVaultData memory openVaultData = abi.decode(data, (OpenVaultData));
 
-        bytes32 vaultId = _openVault(openVaultData);
-        push(vaultId);
-    }
+    bytes32 vaultId = _openVault(openVaultData);
+    push(vaultId);
+  }
 
-    function _openVault(OpenVaultData memory data) internal returns (bytes32) {
-        bytes32 ilk = IJoin(data.joinAddress).ilk();
-        uint256 vaultId = IManager(data.mcdManager).open(ilk, address(this));
+  function _openVault(OpenVaultData memory data) internal returns (bytes32) {
+    bytes32 ilk = IJoin(data.joinAddress).ilk();
+    uint256 vaultId = IManager(data.mcdManager).open(ilk, address(this));
 
-        return bytes32(vaultId);
-    }
+    return bytes32(vaultId);
+  }
 }
