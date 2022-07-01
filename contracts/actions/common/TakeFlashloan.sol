5fb60d392478969a852029bb5970de980c1d1938
Kuba Włodarczyk <fenris85@gmail.com>
2022-07-01 13:00:16 +0200

    Improve DSProxy auth

--- contracts/actions/common/TakeFlashloan.sol
+++ contracts/actions/common/TakeFlashloan.sol
@@ -4,12 +4,12 @@ import { Executable } from "../common/Executable.sol";
 import { ServiceRegistry } from "../../core/ServiceRegistry.sol";
 import { IERC3156FlashBorrower } from "../../interfaces/flashloan/IERC3156FlashBorrower.sol";
 import { IERC3156FlashLender } from "../../interfaces/flashloan/IERC3156FlashLender.sol";
-import { DSProxy } from "../../libs/DS/DSProxy.sol";
 import { FlashloanData } from "../../core/types/Common.sol";
 import { OPERATION_EXECUTOR, DAI } from "../../core/constants/Common.sol";
 import { FLASH_MINT_MODULE } from "../../core/constants/Maker.sol";
+import {ProxyPermission} from"../../libs/DS/ProxyPermission.sol";
 
-contract TakeFlashloan is Executable {
+contract TakeFlashloan is Executable, ProxyPermission {
   ServiceRegistry internal immutable registry;
 
   constructor(ServiceRegistry _registry) {
@@ -17,8 +17,8 @@ contract TakeFlashloan is Executable {
   }
 
   function execute(bytes calldata data, uint8[] memory) external payable override {
-    DSProxy proxy = DSProxy(payable(address(this)));
-    proxy.setOwner(registry.getRegisteredService(OPERATION_EXECUTOR));
+
+    givePermission(registry.getRegisteredService(OPERATION_EXECUTOR));
 
     FlashloanData memory flData = abi.decode(data, (FlashloanData));
     IERC3156FlashLender(registry.getRegisteredService(FLASH_MINT_MODULE)).flashLoan(
@@ -28,6 +28,6 @@ contract TakeFlashloan is Executable {
       data
     );
 
-    proxy.setOwner(msg.sender);
+    removePermission(registry.getRegisteredService(OPERATION_EXECUTOR));
   }
 }
