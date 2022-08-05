02c56b5a49fedd2485bda46d7a5f2fb7578d7086
James Tuckett <james@tuckett.me>
2022-08-05 13:27:45 +0100

    fix: add missing events to common actions

--- contracts/actions/common/SetApproval.sol
+++ contracts/actions/common/SetApproval.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.1;
 import { Executable } from "../common/Executable.sol";
 import { SafeERC20, IERC20 } from "../../libs/SafeERC20.sol";
 import { SetApprovalData } from "../../core/types/Common.sol";
+import { SET_APPROVAL_ACTION } from "../../core/constants/Common.sol";
 
 contract SetApproval is Executable {
   using SafeERC20 for IERC20;
@@ -10,5 +11,7 @@ contract SetApproval is Executable {
   function execute(bytes calldata data, uint8[] memory) external payable override {
     SetApprovalData memory approval = abi.decode(data, (SetApprovalData));
     IERC20(approval.asset).safeApprove(approval.delegator, approval.amount);
+
+    emit Action(SET_APPROVAL_ACTION, bytes32(approval.amount));
   }
 }
