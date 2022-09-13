91fcae974c4c66e74cd2b0f4454baf130d28dc58
sennett <anthony@oazoapps.com>
2022-09-13 15:53:46 +0100

    oops

--- packages/oasis-actions/src/helpers/calculations/generateTestData.ts
+++
@@ -1,6 +0,0 @@
-import { testDataSources } from './test-scenarios/scenarioDataSources'
-import { generateTestScenariosName } from './testDataUtils'
-
-Object.values(testDataSources).forEach(range => {
-  generateTestScenariosName(range)
-})
