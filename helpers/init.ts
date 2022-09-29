43bd4adc45bf857f8846fe6ca4f21884e12108e3
Jakub Swierczek <jakub.swierczek@outlook.com>
2022-09-29 16:22:34 +0200

    Revert "provider"
    This reverts commit b60b881b1fc16e218a49c10edc5146dc152823af.

--- helpers/init.ts
+++ helpers/init.ts
@@ -4,8 +4,8 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { RuntimeConfig } from './types/common'
 
 export default async function init(hre?: HardhatRuntimeEnvironment): Promise<RuntimeConfig> {
-  //const provider = hre ? hre.ethers.provider : (await import('hardhat')).ethers.provider
-  const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545', 2137)
+  const provider = hre ? hre.ethers.provider : (await import('hardhat')).ethers.provider
+  // const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545', 2137)
   const signer = provider.getSigner(0)
   const address = await signer.getAddress()
 
