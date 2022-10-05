524731beaa67dcbaef5334f04d6c4521f6a4061d
Jakub Swierczek <jakub.swierczek@outlook.com>
2022-10-05 11:33:53 +0200

    service registry in create script

--- tasks/createPosition/index.ts
+++ tasks/createPosition/index.ts
@@ -11,109 +11,152 @@ import init from '../../helpers/init'
 import { getOrCreateProxy } from '../../helpers/proxy'
 import { oneInchCallMock } from '../../helpers/swap/OneInchCallMock'
 import { balanceOf } from '../../helpers/utils'
+import { getOneInchCall } from '../../helpers/swap/OneIchCall'
 
 function amountToWei(amount: BigNumber.Value, precision = 18) {
   BigNumber.config({ EXPONENTIAL_AT: 30 })
   return new BigNumber(amount || 0).times(new BigNumber(10).pow(precision))
 }
 
-task('createPosition', 'Create stETH position on AAVE').setAction(async (taskArgs, hre) => {
-  const config = await init(hre)
+task('createPosition', 'Create stETH position on AAVE')
+  .addOptionalParam<string>('serviceRegistry', 'Service Registry address')
+  .addFlag('dummyswap', 'Use dummy swap')
+  .setAction(async (taskArgs, hre) => {
+    const config = await init(hre)
 
-  const mainnetAddresses = {
-    DAI: ADDRESSES.main.DAI,
-    ETH: ADDRESSES.main.ETH,
-    WETH: ADDRESSES.main.WETH,
-    stETH: ADDRESSES.main.stETH,
-    chainlinkEthUsdPriceFeed: ADDRESSES.main.chainlinkEthUsdPriceFeed,
-    aavePriceOracle: ADDRESSES.main.aavePriceOracle,
-    aaveLendingPool: ADDRESSES.main.aave.MainnetLendingPool,
-    operationExecutor: '0x3C1Cb427D20F15563aDa8C249E71db76d7183B6c', // TODO: get from service registry
-  }
-  const aaveLendingPool = new hre.ethers.Contract(
-    ADDRESSES.main.aave.MainnetLendingPool,
-    AAVELendigPoolABI,
-    config.provider,
-  )
-  const aaveDataProvider = new hre.ethers.Contract(
-    ADDRESSES.main.aave.DataProvider,
-    AAVEDataProviderABI,
-    config.provider,
-  )
+    const serviceRegistryAddress = taskArgs.serviceRegistry || process.env.SERVICE_REGISTRY_ADDRESS!
 
-  const proxyAddress = await getOrCreateProxy(config.signer)
+    const serviceRegistryAbi = [
+      {
+        inputs: [
+          {
+            internalType: 'string',
+            name: 'serviceName',
+            type: 'string',
+          },
+        ],
+        name: 'getRegisteredService',
+        outputs: [
+          {
+            internalType: 'address',
+            name: '',
+            type: 'address',
+          },
+        ],
+        stateMutability: 'view',
+        type: 'function',
+      },
+    ]
 
-  const dsProxy = new hre.ethers.Contract(proxyAddress, DSProxyABI, config.provider).connect(
-    config.signer,
-  )
+    const serviceRegistry = await hre.ethers.getContractAt(
+      serviceRegistryAbi,
+      serviceRegistryAddress,
+      config.signer,
+    )
 
-  console.log(`Proxy Address for account: ${proxyAddress}`)
+    const operationExecutorAddress = await serviceRegistry.getRegisteredService(
+      CONTRACT_NAMES.common.OPERATION_EXECUTOR,
+    )
 
-  const depositAmount = amountToWei(new BigNumber(5))
-  const multiply = new BigNumber(2)
-  const slippage = new BigNumber(0.1)
+    const swapAddress = await serviceRegistry.getRegisteredService(CONTRACT_NAMES.common.SWAP)
 
-  const strategyReturn = await strategy.aave.openStEth(
-    {
-      depositAmount,
-      slippage,
-      multiply,
-    },
-    {
-      addresses: mainnetAddresses,
-      provider: config.provider,
-      getSwapData: oneInchCallMock,
-      dsProxy: dsProxy.address,
-    },
-  )
+    const mainnetAddresses = {
+      DAI: ADDRESSES.main.DAI,
+      ETH: ADDRESSES.main.ETH,
+      WETH: ADDRESSES.main.WETH,
+      stETH: ADDRESSES.main.stETH,
+      chainlinkEthUsdPriceFeed: ADDRESSES.main.chainlinkEthUsdPriceFeed,
+      aavePriceOracle: ADDRESSES.main.aavePriceOracle,
+      aaveLendingPool: ADDRESSES.main.aave.MainnetLendingPool,
+      operationExecutor: operationExecutorAddress,
+    }
+    const aaveLendingPool = new hre.ethers.Contract(
+      ADDRESSES.main.aave.MainnetLendingPool,
+      AAVELendigPoolABI,
+      config.provider,
+    )
+    const aaveDataProvider = new hre.ethers.Contract(
+      ADDRESSES.main.aave.DataProvider,
+      AAVEDataProviderABI,
+      config.provider,
+    )
 
-  const operationExecutor = await hre.ethers.getContractAt(
-    CONTRACT_NAMES.common.OPERATION_EXECUTOR,
-    mainnetAddresses.operationExecutor,
-    config.signer,
-  )
+    const proxyAddress = await getOrCreateProxy(config.signer)
 
-  const [txStatus, tx] = await executeThroughProxy(
-    dsProxy.address,
-    {
-      address: mainnetAddresses.operationExecutor,
-      calldata: operationExecutor.interface.encodeFunctionData('executeOp', [
-        strategyReturn.calls,
-        OPERATION_NAMES.common.CUSTOM_OPERATION,
-      ]),
-    },
-    config.signer,
-    depositAmount.toFixed(0),
-    hre,
-  )
+    const dsProxy = new hre.ethers.Contract(proxyAddress, DSProxyABI, config.provider).connect(
+      config.signer,
+    )
 
-  const userAccountData: AAVEAccountData = await aaveLendingPool.getUserAccountData(dsProxy.address)
-  const userStEthReserveData: AAVEReserveData = await aaveDataProvider.getUserReserveData(
-    ADDRESSES.main.stETH,
-    dsProxy.address,
-  )
+    console.log(`Proxy Address for account: ${proxyAddress}`)
 
-  const proxyStEthBalance = await balanceOf(
-    ADDRESSES.main.stETH,
-    dsProxy.address,
-    {
-      config,
-      isFormatted: true,
-    },
-    hre,
-  )
+    const swapData = taskArgs.dummyswap ? oneInchCallMock : getOneInchCall(swapAddress)
+    const depositAmount = amountToWei(new BigNumber(5))
+    const multiply = new BigNumber(2)
+    const slippage = new BigNumber(0.1)
 
-  const proxyEthBalance = await balanceOf(
-    ADDRESSES.main.ETH,
-    dsProxy.address,
-    { config, isFormatted: true },
-    hre,
-  )
+    const strategyReturn = await strategy.aave.openStEth(
+      {
+        depositAmount,
+        slippage,
+        multiply,
+      },
+      {
+        addresses: mainnetAddresses,
+        provider: config.provider,
+        getSwapData: swapData,
+        dsProxy: dsProxy.address,
+      },
+    )
 
-  console.log('userAccountData', userAccountData.totalDebtETH.toString())
-  console.log('userStEthReserveData', userStEthReserveData.currentATokenBalance.toString())
-  console.log('txStatus', txStatus)
-  console.log('txHash', tx.transactionHash)
-  console.log('proxyStEthBalance', proxyStEthBalance.toString())
-  console.log('proxyEthBalance', proxyEthBalance.toString())
-})
+    const operationExecutor = await hre.ethers.getContractAt(
+      CONTRACT_NAMES.common.OPERATION_EXECUTOR,
+      mainnetAddresses.operationExecutor,
+      config.signer,
+    )
+
+    const [txStatus, tx] = await executeThroughProxy(
+      dsProxy.address,
+      {
+        address: mainnetAddresses.operationExecutor,
+        calldata: operationExecutor.interface.encodeFunctionData('executeOp', [
+          strategyReturn.calls,
+          OPERATION_NAMES.common.CUSTOM_OPERATION,
+        ]),
+      },
+      config.signer,
+      depositAmount.toFixed(0),
+      hre,
+    )
+
+    const userAccountData: AAVEAccountData = await aaveLendingPool.getUserAccountData(
+      dsProxy.address,
+    )
+    const userStEthReserveData: AAVEReserveData = await aaveDataProvider.getUserReserveData(
+      ADDRESSES.main.stETH,
+      dsProxy.address,
+    )
+
+    const proxyStEthBalance = await balanceOf(
+      ADDRESSES.main.stETH,
+      dsProxy.address,
+      {
+        config,
+        isFormatted: true,
+      },
+      hre,
+    )
+
+    const proxyEthBalance = await balanceOf(
+      ADDRESSES.main.ETH,
+      dsProxy.address,
+      { config, isFormatted: true },
+      hre,
+    )
+
+    console.log('userAccountData', userAccountData.totalDebtETH.toString())
+    console.log('userStEthReserveData', userStEthReserveData.currentATokenBalance.toString())
+    console.log('txStatus', txStatus)
+    console.log('txHash', tx.transactionHash)
+    console.log('proxyStEthBalance', proxyStEthBalance.toString())
+    console.log('proxyEthBalance', proxyEthBalance.toString())
+  })
