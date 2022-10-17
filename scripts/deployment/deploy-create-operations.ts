83288f3ec2d85724ed75e69347e4f05f000a3ed5
John Doe <bulk.yard@protonmail.com>
2022-10-17 22:21:02 +0200

    chore: Add OpenAAVEPosition and CloseAAVEPosition in Operations Regis… (#104)
    chore: Add OpenAAVEPosition and CloseAAVEPosition in Operations Registry.

--- scripts/deployment/deploy-create-operations.ts
+++ scripts/deployment/deploy-create-operations.ts
@@ -1,27 +1,13 @@
-import { OPERATION_NAMES } from '@oasisdex/oasis-actions'
-import { CONTRACT_NAMES } from '@oasisdex/oasis-actions/src/helpers/constants'
+import { CONTRACT_NAMES, OPERATION_NAMES } from '@oasisdex/oasis-actions'
 import { utils } from 'ethers'
 import hre from 'hardhat'
 
-import {
-  CdpAllow,
-  MakerDeposit,
-  MakerGenerate,
-  MakerOpenVault,
-  MakerPayback,
-  MakerWithdraw,
-  PullToken,
-  SendToken,
-  SetApproval,
-  SwapAction,
-  TakeFlashloan,
-} from '../../typechain'
-import { HardhatUtils } from '../common'
-import { getServiceNameHash } from '../common/utils'
+import { getServiceNameHash, HardhatUtils } from '../common'
 
 function operationLabelHash(label: string): string {
   return utils.keccak256(utils.toUtf8Bytes(label))
 }
+
 async function main() {
   const utils = new HardhatUtils(hre) // the hardhat network is coalesced to mainnet
   const signer = hre.ethers.provider.getSigner(0)
@@ -29,11 +15,47 @@ async function main() {
   console.log(`Deployer address: ${await signer.getAddress()}`)
   console.log(`Network: ${network}`)
 
-  const gasSettings = await utils.getGasSettings()
   const system = await utils.getDefaultSystem()
 
-  console.log(`Adding CUSTOM_OPERATION to OperationsRegistry....`)
+  console.log('OperationsRegistry Address:', system.operationsRegistry.address)
+
+  console.log(`Adding ${OPERATION_NAMES.common.CUSTOM_OPERATION} to OperationsRegistry...`)
   await system.operationsRegistry.addOperation(OPERATION_NAMES.common.CUSTOM_OPERATION, [])
+
+  console.log(`Adding ${OPERATION_NAMES.aave.OPEN_POSITION} to OperationsRegistry...`)
+  const openAPositionActions = [
+    getServiceNameHash(CONTRACT_NAMES.common.TAKE_A_FLASHLOAN),
+    getServiceNameHash(CONTRACT_NAMES.common.SET_APPROVAL),
+    getServiceNameHash(CONTRACT_NAMES.aave.DEPOSIT),
+    getServiceNameHash(CONTRACT_NAMES.aave.BORROW),
+    getServiceNameHash(CONTRACT_NAMES.common.WRAP_ETH),
+    getServiceNameHash(CONTRACT_NAMES.common.SWAP_ACTION),
+    getServiceNameHash(CONTRACT_NAMES.common.SET_APPROVAL),
+    getServiceNameHash(CONTRACT_NAMES.aave.DEPOSIT),
+    getServiceNameHash(CONTRACT_NAMES.aave.WITHDRAW),
+  ]
+  await system.operationsRegistry.addOperation(
+    OPERATION_NAMES.aave.OPEN_POSITION,
+    openAPositionActions,
+  )
+
+  const closeAPositionActions = [
+    getServiceNameHash(CONTRACT_NAMES.common.TAKE_A_FLASHLOAN),
+    getServiceNameHash(CONTRACT_NAMES.common.SET_APPROVAL),
+    getServiceNameHash(CONTRACT_NAMES.aave.DEPOSIT),
+    getServiceNameHash(CONTRACT_NAMES.aave.WITHDRAW),
+    getServiceNameHash(CONTRACT_NAMES.common.SWAP_ACTION),
+    getServiceNameHash(CONTRACT_NAMES.common.SET_APPROVAL),
+    getServiceNameHash(CONTRACT_NAMES.aave.PAYBACK),
+    getServiceNameHash(CONTRACT_NAMES.aave.WITHDRAW),
+    getServiceNameHash(CONTRACT_NAMES.common.UNWRAP_ETH),
+    getServiceNameHash(CONTRACT_NAMES.common.RETURN_FUNDS),
+  ]
+  console.log(`Adding ${OPERATION_NAMES.aave.CLOSE_POSITION} to OperationsRegistry...`)
+  await system.operationsRegistry.addOperation(
+    OPERATION_NAMES.aave.CLOSE_POSITION,
+    closeAPositionActions,
+  )
 }
 
 main().catch(error => {
