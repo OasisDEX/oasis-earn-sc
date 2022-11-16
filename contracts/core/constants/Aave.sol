badd573220834fe6dd4fb8bb57bd9194f745dea2
James Tuckett <james@tuckett.me>
2022-11-16 10:58:00 +0000

    refactor: add new major version to contract names that are emitted by events

--- contracts/core/constants/Aave.sol
+++ contracts/core/constants/Aave.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.15;
 string constant AAVE_LENDING_POOL = "AaveLendingPool";
 string constant AAVE_WETH_GATEWAY = "AaveWethGateway";
 
-string constant BORROW_ACTION = "AaveBorrow";
-string constant DEPOSIT_ACTION = "AaveDeposit";
-string constant WITHDRAW_ACTION = "AaveWithdraw";
-string constant PAYBACK_ACTION = "AavePayback";
+string constant BORROW_ACTION = "AaveBorrow_2_0_0";
+string constant DEPOSIT_ACTION = "AaveDeposit_2_0_0";
+string constant WITHDRAW_ACTION = "AaveWithdraw_2_0_0";
+string constant PAYBACK_ACTION = "AavePayback_2_0_0";
--- contracts/core/constants/Common.sol
+++ contracts/core/constants/Common.sol
@@ -9,13 +9,14 @@ string constant DAI = "DAI";
 uint256 constant RAY = 10**27;
 bytes32 constant NULL = "";
 
-string constant PULL_TOKEN_ACTION = "PullToken";
-string constant SEND_TOKEN_ACTION = "SendToken";
-string constant SET_APPROVAL_ACTION = "SetApproval";
-string constant TAKE_FLASH_LOAN_ACTION = "TakeFlashloan";
-string constant WRAP_ETH = "WrapEth";
-string constant UNWRAP_ETH = "UnwrapEth";
-string constant RETURN_FUNDS_ACTION = "ReturnFunds";
+string constant PULL_TOKEN_ACTION = "PullToken_2_0_0";
+string constant SEND_TOKEN_ACTION = "SendToken_2_0_0";
+string constant SET_APPROVAL_ACTION = "SetApproval_2_0_0";
+string constant TAKE_FLASH_LOAN_ACTION = "TakeFlashloan_2_0_0";
+string constant WRAP_ETH = "WrapEth_2_0_0";
+string constant UNWRAP_ETH = "UnwrapEth_2_0_0";
+string constant RETURN_FUNDS_ACTION = "ReturnFunds_2_0_0";
+string constant PULL_TO_PROXY_ACTION = "PullToProxy_2_0_0";
 
 string constant UNISWAP_ROUTER = "UniswapRouter";
 string constant SWAP = "Swap";
