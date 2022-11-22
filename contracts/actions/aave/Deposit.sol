9b1ea9771102c14798d7df4059f3df590f2d578a
Jakub Swierczek <jakub.swierczek@outlook.com>
2022-11-22 12:40:35 +0100

    Reopen position in AAVE

--- contracts/actions/aave/Deposit.sol
+++ contracts/actions/aave/Deposit.sol
@@ -35,6 +35,11 @@ contract AaveDeposit is Executable, UseStore {
       0
     );
 
+    ILendingPool(registry.getRegisteredService(AAVE_LENDING_POOL)).setUserUseReserveAsCollateral(
+      deposit.asset,
+      true
+    );
+
     store().write(bytes32(deposit.amount));
     emit Action(DEPOSIT_ACTION, bytes32(deposit.amount));
   }
