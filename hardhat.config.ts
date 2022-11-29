8fb55b6053c3bce083a61e41cb2ce54a8df85209
Kuba Wlodarczyk <fenris85@gmail.com>
2022-11-29 20:28:53 +0100

    Add abi generate for hh

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,6 +10,7 @@ import './tasks/proxy'
 import './tasks/verify-earn'
 import 'solidity-docgen'
 import 'hardhat-tracer'
+import 'hardhat-abi-exporter'
 
 import { default as dotenv } from 'dotenv'
 import { HardhatUserConfig, task } from 'hardhat/config'
@@ -125,6 +126,14 @@ const config: HardhatUserConfig = {
       './test',
     ],
   },
+  abiExporter: {
+    path: './abi/generated',
+    runOnCompile: true,
+    clear: true,
+    flat: true,
+    spacing: 2,
+    pretty: true,
+  }
 }
 
 export default config
