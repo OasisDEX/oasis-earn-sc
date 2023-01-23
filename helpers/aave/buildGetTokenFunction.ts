3465854084aef2dcd59c248c1229e41453399689
John Doe <bulk.yard@protonmail.com>
2023-01-23 15:34:55 +0100

    Replace hardhatInit with the standard init (#170)

--- helpers/aave/buildGetTokenFunction.ts
+++ helpers/aave/buildGetTokenFunction.ts
@@ -1,8 +1,9 @@
 import { AAVETokens } from '@oasisdex/oasis-actions'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
 
 import erc20abi from '../../abi/external/IERC20.json'
 import { mainnetAddresses } from '../../test/addresses'
-import { HardhatRuntimeConfig } from '../types/common'
+import { RuntimeConfig } from '../types/common'
 
 export type AAVETokensToGet = Exclude<AAVETokens, 'ETH' | 'WETH'>
 const tokensWhales: Record<AAVETokensToGet, { whale: string; tokenAddress: string }> = {
@@ -21,27 +22,28 @@ const tokensWhales: Record<AAVETokensToGet, { whale: string; tokenAddress: strin
 }
 
 export function buildGetTokenFunction(
-  config: HardhatRuntimeConfig,
+  config: RuntimeConfig,
+  hre: HardhatRuntimeEnvironment,
 ): (symbol: AAVETokensToGet, amount: string) => Promise<boolean> {
   return async function getTokens(symbol: AAVETokensToGet, amount: string): Promise<boolean> {
     const { tokenAddress, whale } = tokensWhales[symbol]
-    const fromSigner = await config.ethers.getSigner(whale)
+    const fromSigner = await hre.ethers.getSigner(whale)
 
     await config.signer.sendTransaction({
       from: await config.signer.getAddress(),
       to: whale,
-      value: config.ethers.utils.parseEther('1'),
-      gasLimit: config.ethers.utils.hexlify(1000000),
+      value: hre.ethers.utils.parseEther('1'),
+      gasLimit: hre.ethers.utils.hexlify(1000000),
     })
 
-    await config.network.provider.request({
+    await hre.network.provider.request({
       method: 'hardhat_impersonateAccount',
       params: [whale],
     })
 
-    const fromTokenContract = new config.ethers.Contract(tokenAddress, erc20abi, fromSigner)
+    const fromTokenContract = new hre.ethers.Contract(tokenAddress, erc20abi, fromSigner)
 
-    await fromTokenContract.transfer(config.address, config.ethers.utils.parseUnits(amount, 'wei'))
+    await fromTokenContract.transfer(config.address, hre.ethers.utils.parseUnits(amount, 'wei'))
 
     return true
   }
