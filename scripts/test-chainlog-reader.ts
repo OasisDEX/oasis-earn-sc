22e78df9eb778860c06d77bf6f689151fdb8f4d7
halaprix <panicz.klos@gmail.com>
2023-02-28 22:41:07 +0100

    add test for `getIlkJoinAddressByHash`

--- scripts/test-chainlog-reader.ts
+++ scripts/test-chainlog-reader.ts
@@ -1,3 +1,4 @@
+import { expect } from 'chai'
 
 import { createDeploy } from '../helpers/deploy'
 import init from '../helpers/init'
@@ -12,14 +13,20 @@ async function main() {
 
   const deploy = await createDeploy(options)
 
-  const chainlogAddress = "0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F"
-  const [chainLogView, ] = await deploy("ChainLogView", [chainlogAddress])
+  const chainlogAddress = '0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F'
+  const [chainLogView] = await deploy('ChainLogView', [chainlogAddress])
 
-  const cdpManagerAddress = await chainLogView.getServiceAddress("CDP_MANAGER")
-  const ethAJoinAddress = await chainLogView.getIlkJoinAddressByName("ETH_A")
+  const cdpManagerAddress = await chainLogView.getServiceAddress('CDP_MANAGER')
+  const ethAJoinAddress = await chainLogView.getIlkJoinAddressByName('ETH_A')
+  const ethAJoinAddressFromHash = await chainLogView.getIlkJoinAddressByHash(
+    '0x4554485f41000000000000000000000000000000000000000000000000000000',
+  )
 
-  console.log("cdpManagerAddress", cdpManagerAddress)
-  console.log("EthAJoinAddress", ethAJoinAddress)
+  console.log('cdpManagerAddress', cdpManagerAddress)
+  console.log('EthAJoinAddress', ethAJoinAddress)
+  console.log('EthAJoinAddressFromHash', ethAJoinAddressFromHash)
+
+  expect(ethAJoinAddress).to.equal(ethAJoinAddressFromHash)
 }
 
 main().catch(error => {
