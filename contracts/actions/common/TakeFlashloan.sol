7c6b4f493d945b4f2d439acd4132f611106f3f25
James Tuckett <james@tuckett.me>
2023-03-13 13:13:21 +0000

    chore: fix formatting

--- contracts/actions/common/TakeFlashloan.sol
+++ contracts/actions/common/TakeFlashloan.sol
@@ -1,97 +1,101 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.15;
 
-import {Executable} from "../common/Executable.sol";
-import {ServiceRegistry} from "../../core/ServiceRegistry.sol";
-import {IVault} from "../../interfaces/balancer/IVault.sol";
-import {IERC3156FlashBorrower} from "../../interfaces/flashloan/IERC3156FlashBorrower.sol";
-import {IERC3156FlashLender} from "../../interfaces/flashloan/IERC3156FlashLender.sol";
-import {IFlashLoanRecipient} from "../../interfaces/flashloan/balancer/IFlashLoanRecipient.sol";
-import {FlashloanData, FlashloanWithInitiatorData, FlashloanProvider} from "../../core/types/Common.sol";
+import { Executable } from "../common/Executable.sol";
+import { ServiceRegistry } from "../../core/ServiceRegistry.sol";
+import { IVault } from "../../interfaces/balancer/IVault.sol";
+import { IERC3156FlashBorrower } from "../../interfaces/flashloan/IERC3156FlashBorrower.sol";
+import { IERC3156FlashLender } from "../../interfaces/flashloan/IERC3156FlashLender.sol";
+import { IFlashLoanRecipient } from "../../interfaces/flashloan/balancer/IFlashLoanRecipient.sol";
 import {
-OPERATION_EXECUTOR,
-DAI,
-TAKE_FLASH_LOAN_ACTION,
-CHAINLOG_VIEWER
+  FlashloanData,
+  FlashloanWithInitiatorData,
+  FlashloanProvider
+} from "../../core/types/Common.sol";
+import {
+  OPERATION_EXECUTOR,
+  DAI,
+  TAKE_FLASH_LOAN_ACTION,
+  CHAINLOG_VIEWER
 } from "../../core/constants/Common.sol";
-import {MCD_FLASH} from "../../core/constants/Maker.sol";
-import {BALANCER_VAULT} from "../../core/constants/Balancer.sol";
-import {ChainLogView} from "../../core/views/ChainLogView.sol";
-import {ProxyPermission} from "../../libs/DS/ProxyPermission.sol";
-import {IERC20} from "../../libs/SafeERC20.sol";
+import { MCD_FLASH } from "../../core/constants/Maker.sol";
+import { BALANCER_VAULT } from "../../core/constants/Balancer.sol";
+import { ChainLogView } from "../../core/views/ChainLogView.sol";
+import { ProxyPermission } from "../../libs/DS/ProxyPermission.sol";
+import { IERC20 } from "../../libs/SafeERC20.sol";
 
 /**
  * @title TakeFlashloan Action contract
  * @notice Executes a sequence of Actions after flashloaning funds
  */
 contract TakeFlashloan is Executable, ProxyPermission {
-    ServiceRegistry internal immutable registry;
-    address internal immutable dai;
+  ServiceRegistry internal immutable registry;
+  address internal immutable dai;
 
-    constructor(ServiceRegistry _registry, address _dai) {
-        registry = _registry;
-        dai = _dai;
-    }
+  constructor(ServiceRegistry _registry, address _dai) {
+    registry = _registry;
+    dai = _dai;
+  }
 
-    /**
-     * @dev When the Flashloan lender calls back the Operation Executor we may need to re-establish the calling context.
+  /**
+   * @dev When the Flashloan lender calls back the Operation Executor we may need to re-establish the calling context.
    * @dev The isProxyFlashloan flag is used to give the Operation Executor temporary authority to call the execute method on a user"s proxy. Refers to any proxy wallet (DSProxy or DPMProxy at time of writing)
    * @dev isDPMProxy flag switches between regular DSPRoxy and DPMProxy
    * @param data Encoded calldata that conforms to the FlashloanData struct
    */
-    function execute(bytes calldata data, uint8[] memory) external payable override {
-        FlashloanData memory flDataFromProps = parseInputs(data);
-        // We need to pass the initiator address to the Operation Executor so that it can re-establish the calling context
-        FlashloanWithInitiatorData memory flData = FlashloanWithInitiatorData({
-        amount : flDataFromProps.amount,
-        asset : flDataFromProps.asset,
-        isProxyFlashloan : flDataFromProps.isProxyFlashloan,
-        isDPMProxy : flDataFromProps.isDPMProxy,
-        provider : flDataFromProps.provider,
-        calls : flDataFromProps.calls,
-        initiator : address(this)
-        });
+  function execute(bytes calldata data, uint8[] memory) external payable override {
+    FlashloanData memory flDataFromProps = parseInputs(data);
+    // We need to pass the initiator address to the Operation Executor so that it can re-establish the calling context
+    FlashloanWithInitiatorData memory flData = FlashloanWithInitiatorData({
+      amount: flDataFromProps.amount,
+      asset: flDataFromProps.asset,
+      isProxyFlashloan: flDataFromProps.isProxyFlashloan,
+      isDPMProxy: flDataFromProps.isDPMProxy,
+      provider: flDataFromProps.provider,
+      calls: flDataFromProps.calls,
+      initiator: address(this)
+    });
 
-        address operationExecutorAddress = registry.getRegisteredService(OPERATION_EXECUTOR);
+    address operationExecutorAddress = registry.getRegisteredService(OPERATION_EXECUTOR);
 
-        if (flData.isProxyFlashloan) {
-            givePermission(flData.isDPMProxy, operationExecutorAddress);
-        }
-
-        if (flData.provider == FlashloanProvider.DssFlash) {
-            ChainLogView chainlogView = ChainLogView(registry.getRegisteredService(CHAINLOG_VIEWER));
-
-            IERC3156FlashLender(chainlogView.getServiceAddress(MCD_FLASH)).flashLoan(
-                IERC3156FlashBorrower(operationExecutorAddress),
-                dai,
-                flData.amount,
-                abi.encode(flData)
-            );
-        }
-
-        if (flData.provider == FlashloanProvider.Balancer) {
-            IERC20[] memory tokens = new IERC20[](1);
-            uint256[] memory amounts = new uint256[](1);
-
-            tokens[0] = IERC20(flData.asset);
-            amounts[0] = flData.amount;
-
-            IVault(registry.getRegisteredService(BALANCER_VAULT)).flashLoan(
-                IFlashLoanRecipient(operationExecutorAddress),
-                tokens,
-                amounts,
-                abi.encode(flData)
-            );
-        }
-
-        if (flData.isProxyFlashloan) {
-            removePermission(flData.isDPMProxy, operationExecutorAddress);
-        }
-
-        emit Action(TAKE_FLASH_LOAN_ACTION, bytes(abi.encode(flData.amount)));
+    if (flData.isProxyFlashloan) {
+      givePermission(flData.isDPMProxy, operationExecutorAddress);
     }
 
-    function parseInputs(bytes memory _callData) public pure returns (FlashloanData memory params) {
-        return abi.decode(_callData, (FlashloanData));
+    if (flData.provider == FlashloanProvider.DssFlash) {
+      ChainLogView chainlogView = ChainLogView(registry.getRegisteredService(CHAINLOG_VIEWER));
+
+      IERC3156FlashLender(chainlogView.getServiceAddress(MCD_FLASH)).flashLoan(
+        IERC3156FlashBorrower(operationExecutorAddress),
+        dai,
+        flData.amount,
+        abi.encode(flData)
+      );
     }
+
+    if (flData.provider == FlashloanProvider.Balancer) {
+      IERC20[] memory tokens = new IERC20[](1);
+      uint256[] memory amounts = new uint256[](1);
+
+      tokens[0] = IERC20(flData.asset);
+      amounts[0] = flData.amount;
+
+      IVault(registry.getRegisteredService(BALANCER_VAULT)).flashLoan(
+        IFlashLoanRecipient(operationExecutorAddress),
+        tokens,
+        amounts,
+        abi.encode(flData)
+      );
+    }
+
+    if (flData.isProxyFlashloan) {
+      removePermission(flData.isDPMProxy, operationExecutorAddress);
+    }
+
+    emit Action(TAKE_FLASH_LOAN_ACTION, bytes(abi.encode(flData.amount)));
+  }
+
+  function parseInputs(bytes memory _callData) public pure returns (FlashloanData memory params) {
+    return abi.decode(_callData, (FlashloanData));
+  }
 }
