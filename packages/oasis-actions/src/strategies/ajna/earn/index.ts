f34e930e3191fd23ee64fc770d072fbb231657a5
James Tuckett <james@tuckett.me>
2023-03-15 15:20:56 +0000

    chore: addressing conflicts post merge

--- packages/oasis-actions/src/strategies/ajna/earn/index.ts
+++
@@ -1,9 +0,0 @@
-import { depositAndAdjust } from './depositAndAdjust'
-import { open } from './open'
-import { withdrawAndAdjust } from './withdrawAndAdjust'
-
-export const earn = {
-  open,
-  depositAndAdjust,
-  withdrawAndAdjust,
-}
