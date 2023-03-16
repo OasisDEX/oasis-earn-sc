1065fa344aeecb2e2db65dd016279ee883f8dc6e
James Tuckett <james@tuckett.me>
2023-03-16 09:41:11 +0000

    Split tests into Unit & E2E in /tests directory (#220)
    * refactor: move Swap contracts into their own directory. Move Unit and E2E tests into their own directories
    
    * refactor: merge dev and restructure remaining outlier tests

--- contracts/actions/common/SwapAction.sol
+++ contracts/actions/common/SwapAction.sol
@@ -7,7 +7,7 @@ import { SafeERC20, IERC20 } from "../../libs/SafeERC20.sol";
 import { IWETH } from "../../interfaces/tokens/IWETH.sol";
 import { SwapData } from "../../core/types/Common.sol";
 import { UseStore, Write } from "../../actions/common/UseStore.sol";
-import { Swap } from "./Swap.sol";
+import { Swap } from "../../swap/Swap.sol";
 import { WETH, SWAP } from "../../core/constants/Common.sol";
 import { OperationStorage } from "../../core/OperationStorage.sol";
 import { SWAP } from "../../core/constants/Common.sol";
