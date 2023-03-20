23d8989f69dc5573e9fef7fa3ba86e9be1294268
Damian Paszkowski <paszko.damian@gmail.com>
2023-03-20 13:28:29 +0100

    fix dust validation

--- packages/oasis-actions/src/types/ajna/validation/dustLimit.ts
+++ packages/oasis-actions/src/types/ajna/validation/dustLimit.ts
@@ -2,7 +2,7 @@ import { AjnaError } from '../../common'
 import { AjnaPosition } from '../AjnaPosition'
 
 export function validateDustLimit(position: AjnaPosition): AjnaError[] {
-  if (position.debtAmount.gte(position.pool.poolMinDebtAmount)) {
+  if (position.debtAmount.lt(position.pool.poolMinDebtAmount)) {
     return [
       {
         name: 'debt-less-then-dust-limit',
