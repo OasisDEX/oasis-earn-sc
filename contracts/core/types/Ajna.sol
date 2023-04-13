ca56e5711dbd35ec484e9cefc09e3ceb8a1cfa63
Damian Paszkowski <paszko.damian@gmail.com>
2023-04-13 14:49:50 +0200

    add deposit borrow action

---
+++ contracts/core/types/Ajna.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.15;
+
+struct DepositBorrowData {
+  address pool;
+  uint256 depositAmount;
+  uint256 borrowAmount;
+  uint256 price;
+}
+
+struct RepayWithdrawData {
+  address pool;
+  uint256 depositAmount;
+  uint256 borrowAmount;
+  uint256 price;
+}
