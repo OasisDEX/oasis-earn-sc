92e9b7971c83e6920ad21ae523e8c9b660f54d0a
James Tuckett <james@tuckett.me>
2023-05-03 15:18:15 +0100

    chore: tweak position creation retry util to make it more explicit what's failed and what succeeded

--- packages/dma-contracts/test/utils/aave/create-position-with-retries.ts
+++ packages/dma-contracts/test/utils/aave/create-position-with-retries.ts
@@ -1,18 +1,26 @@
 import { advanceBlocks, advanceTime } from '@dma-common/test-utils'
+import { PositionDetails } from '@dma-contracts/test/fixtures'
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 
 const POSITION_RETRIES = 3
 const BLOCKS_TO_ADVANCE = 5
 const TIME_TO_ADVANCE = 60
 
-export async function createPositionWithRetries<A, P>(
+export async function createPositionWithRetries<A, P extends PositionDetails | null>(
   ethers: HardhatRuntimeEnvironment['ethers'],
   positionCreationFunction: (args: A) => Promise<P>,
   args: A,
 ): Promise<P> {
   for (let attempt = 0; attempt < POSITION_RETRIES; attempt++) {
     try {
-      return await positionCreationFunction(args)
+      const position = await positionCreationFunction(args)
+      position &&
+        console.info(
+          `${position.strategy} Position created after ${attempt + 1} attempt${
+            attempt + 1 > 1 ? 's' : ''
+          }`,
+        )
+      return position
     } catch (error) {
       console.warn(`Position creation failed, attempt ${attempt + 1}:`, error)
 
