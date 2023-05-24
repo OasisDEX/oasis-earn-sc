0bd075306c36bf5673273203ef46020f1302febe
James Tuckett <james@tuckett.me>
2023-05-24 14:57:47 +0100

    refactor: extract flashloan calculations and adjust logic to their own modules

---
+++ packages/domain/src/flashloans.ts
@@ -0,0 +1,5 @@
+/** For example, flashloaning DAI to open an ETH/USDC position on AAVE */
+export const transientCollateralFlashloan = () => {}
+
+/** For example, flashloaning USDC to open an ETH/USDC position on Ajna */
+export const debtToCollateralSwapFlashloan = () => {}
