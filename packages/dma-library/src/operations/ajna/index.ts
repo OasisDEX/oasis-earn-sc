1a8ece2d1a18c5dc50e32f5296667fcf5f37d0aa
piekczyk <piekarczyk.sebastian@gmail.com>
2023-06-01 14:40:35 +0200

    Ajna adjust risk down operation (#333)
    * Ajna adjust risk down operation
    
    * changed order
    
    * fix
    
    * fix
    
    * fix
    
    * fix order
    
    * unwrap skipped
    
    * export fix
    
    * order of params adjusted
    
    * calls adjusted

--- packages/dma-library/src/operations/ajna/index.ts
+++ packages/dma-library/src/operations/ajna/index.ts
@@ -1,13 +1,16 @@
 export { AjnaStrategyAddresses } from './addresses'
+import { adjustRiskDown, AjnaAdjustRiskDownOperation } from './adjust-risk-down'
 import { adjustRiskUp, AjnaAdjustRiskUpOperation } from './adjust-risk-up'
 import { AjnaOpenOperation, open } from './open'
 
 export type AjnaOperations = {
   open: AjnaOpenOperation
   adjustRiskUp: AjnaAdjustRiskUpOperation
+  adjustRiskDown: AjnaAdjustRiskDownOperation
 }
 
 export const ajnaOperations: AjnaOperations = {
   open,
   adjustRiskUp,
+  adjustRiskDown,
 }
