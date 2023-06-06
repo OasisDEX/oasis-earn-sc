226b77895c0011ae20d75039209883ae60ec5fb2
halaprix <panicz.klos@gmail.com>
2023-06-06 16:31:11 +0200

    fix: add `0.8.18` solidity version to hh config

--- packages/dma-contracts/hardhat.config.ts
+++ packages/dma-contracts/hardhat.config.ts
@@ -125,6 +125,15 @@ const config = {
           },
         },
       },
+      {
+        version: '0.8.18',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 0,
+          },
+        },
+      },
     ],
     settings: {
       optimizer: {
