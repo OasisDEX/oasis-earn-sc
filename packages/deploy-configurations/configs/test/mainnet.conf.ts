a4d377e6716eb019c34c2ae03e6587dc8aa2e8f7
James Tuckett <james@tuckett.me>
2023-07-19 12:18:34 +0100

    Deploy Ajna DMA Actions & Add entries to Service/Operation Registries (#385)
    * chore: add ERC20PoolFactory to deploy configs
    
    * chore: generate new Ajna action addresses and update deployer to skip auto gnosis sr additions
    
    * chore: fix lint issues

--- packages/deploy-configurations/configs/test/mainnet.conf.ts
+++ packages/deploy-configurations/configs/test/mainnet.conf.ts
@@ -1022,7 +1022,7 @@ export const config: SystemConfig = {
     },
     ERC20PoolFactory: {
       name: 'ERC20PoolFactory',
-      address: '0xe6F4d9711121e5304b30aC2Aae57E3b085ad3c4d',
+      address: '0xe6f4d9711121e5304b30ac2aae57e3b085ad3c4d',
       serviceRegistryName: SERVICE_REGISTRY_NAMES.ajna.ERC20_POOL_FACTORY,
     },
   },
