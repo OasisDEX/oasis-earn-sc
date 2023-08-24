a1bbe8a21dee30cea0e8b12605f0d0997a3954ff
James Tuckett <james@tuckett.me>
2023-08-24 09:48:10 +0100

    refactor: Aave V3 Withdraw to Spark Withdraw

--- packages/dma-contracts/contracts/actions/spark/Withdraw.sol
+++ packages/dma-contracts/contracts/actions/spark/Withdraw.sol
@@ -1,19 +1,18 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.15;
 
-import { Executable } from "../../common/Executable.sol";
-import { UseStore, Write } from "../../common/UseStore.sol";
-import { OperationStorage } from "../../../core/OperationStorage.sol";
-import { ILendingPool } from "../../../interfaces/aave/ILendingPool.sol";
-import { WithdrawData } from "../../../core/types/Aave.sol";
-import { AAVE_POOL } from "../../../core/constants/Aave.sol";
-import { IPoolV3 } from "../../../interfaces/aaveV3/IPoolV3.sol";
+import { Executable } from "../common/Executable.sol";
+import { Write, UseStore } from "../common/UseStore.sol";
+import { OperationStorage } from "../../core/OperationStorage.sol";
+import { WithdrawData } from "../../core/types/Spark.sol";
+import { SPARK_LENDING_POOL } from "../../core/constants/Spark.sol";
+import { IPool } from "../../interfaces/spark/IPool.sol";
 
 /**
- * @title Withdraw | AAVE V3 Action contract
- * @notice Withdraw collateral from AAVE's lending pool
+ * @title Withdraw | Spark Action contract
+ * @notice Withdraw collateral from Spark's lending pool
  */
-contract AaveV3Withdraw is Executable, UseStore {
+contract SparkWithdraw is Executable, UseStore {
   using Write for OperationStorage;
 
   constructor(address _registry) UseStore(_registry) {}
@@ -24,7 +23,7 @@ contract AaveV3Withdraw is Executable, UseStore {
   function execute(bytes calldata data, uint8[] memory) external payable override {
     WithdrawData memory withdraw = parseInputs(data);
 
-    uint256 amountWithdrawn = IPoolV3(registry.getRegisteredService(AAVE_POOL)).withdraw(
+    uint256 amountWithdrawn = IPool(registry.getRegisteredService(SPARK_LENDING_POOL)).withdraw(
       withdraw.asset,
       withdraw.amount,
       withdraw.to
