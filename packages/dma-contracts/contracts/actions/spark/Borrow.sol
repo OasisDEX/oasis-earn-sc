0a5fb417cac8fca3dee307c352c1e5bcca9543eb
James Tuckett <james@tuckett.me>
2023-08-24 09:37:39 +0100

    chore: add Spark types and interfaces

--- packages/dma-contracts/contracts/actions/spark/Borrow.sol
+++ packages/dma-contracts/contracts/actions/spark/Borrow.sol
@@ -4,11 +4,8 @@ pragma solidity ^0.8.15;
 import { Executable } from "../../common/Executable.sol";
 import { Write, UseStore } from "../../common/UseStore.sol";
 import { OperationStorage } from "../../../core/OperationStorage.sol";
-import { IVariableDebtToken } from "../../../interfaces/aave/IVariableDebtToken.sol";
-import { IWETHGateway } from "../../../interfaces/aave/IWETHGateway.sol";
-import { ILendingPool } from "../../../interfaces/aave/ILendingPool.sol";
-import { BorrowData } from "../../../core/types/Aave.sol";
-import { AAVE_POOL } from "../../../core/constants/Aave.sol";
+import { BorrowData } from "../../../core/types/Spark.sol";
+import { SPARK_LENDING_POOL } from "../../../core/constants/Spark.sol";
 import { IPoolV3 } from "../../../interfaces/aaveV3/IPoolV3.sol";
 
 /**
