441c21fec9a3960f7fa8d3d60f6b0dcbdb07c74c
James Tuckett <james@tuckett.me>
2023-08-24 09:46:42 +0100

    refactor: Aave V3 EMode to Spark EMode

--- packages/dma-contracts/contracts/actions/spark/SetEMode.sol
+++ packages/dma-contracts/contracts/actions/spark/SetEMode.sol
@@ -1,21 +1,18 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.15;
 
-import { Executable } from "../../common/Executable.sol";
-import { Write, UseStore } from "../../common/UseStore.sol";
-import { OperationStorage } from "../../../core/OperationStorage.sol";
-import { IVariableDebtToken } from "../../../interfaces/aave/IVariableDebtToken.sol";
-import { IWETHGateway } from "../../../interfaces/aave/IWETHGateway.sol";
-import { ILendingPool } from "../../../interfaces/aave/ILendingPool.sol";
-import { SetEModeData } from "../../../core/types/Aave.sol";
-import { AAVE_POOL } from "../../../core/constants/Aave.sol";
-import { IPoolV3 } from "../../../interfaces/aaveV3/IPoolV3.sol";
+import { Executable } from "../common/Executable.sol";
+import { Write, UseStore } from "../common/UseStore.sol";
+import { OperationStorage } from "../../core/OperationStorage.sol";
+import { SetEModeData } from "../../core/types/Spark.sol";
+import { SPARK_LENDING_POOL } from "../../core/constants/Spark.sol";
+import { IPool } from "../../interfaces/spark/IPool.sol";
 
 /**
- * @title SetEMode | AAVE V3 Action contract
- * @notice Sets the user's eMode on AAVE's lending pool
+ * @title SetEMode | Spark Action contract
+ * @notice Sets the user's eMode on Spark's lending pool
  */
-contract AaveV3SetEMode is Executable, UseStore {
+contract SparkSetEMode is Executable, UseStore {
   using Write for OperationStorage;
 
   constructor(address _registry) UseStore(_registry) {}
@@ -26,7 +23,7 @@ contract AaveV3SetEMode is Executable, UseStore {
   function execute(bytes calldata data, uint8[] memory) external payable override {
     SetEModeData memory emode = parseInputs(data);
 
-    IPoolV3(registry.getRegisteredService(AAVE_POOL)).setUserEMode(emode.categoryId);
+    IPool(registry.getRegisteredService(SPARK_LENDING_POOL)).setUserEMode(emode.categoryId);
 
     store().write(bytes32(uint256(emode.categoryId)));
   }
