e22d1c5fc41d3c4411cf1f0b3ea9b6b676e6ec0e
James Tuckett <james@tuckett.me>
2023-08-24 12:53:30 +0100

    chore: address linting issues

--- packages/deploy-configurations/types/deployment-config/index.ts
+++ packages/deploy-configurations/types/deployment-config/index.ts
@@ -1,2 +1,16 @@
+export { AaveLikeProtocol } from './aave-like-protocol'
+export { AaveV2ProtocolContracts, AaveV3ProtocolContracts } from './aave-protocol'
+export { ActionContracts, Actions } from './actions'
+export { AjnaProtocol, AjnaProtocolContracts } from './ajna-protocol'
+export { Automation, AutomationContracts } from './automation'
+export { Common, CommonContracts } from './common'
 export { ConfigEntry, Contracts, SystemContracts } from './config-entries'
+export { CoreContracts } from './core'
 export { SystemConfig } from './deployment-config'
+export {
+  MakerProtocolCommonContracts,
+  MakerProtocolJoinContracts,
+  MakerProtocolPipContracts,
+} from './maker-protocol'
+export { SystemKeys } from './system-keys'
+export { Tokens } from './tokens'
