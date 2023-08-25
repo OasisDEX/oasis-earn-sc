6aaa912048a76b30a8857b09f23a53249091ace2
James Tuckett <james@tuckett.me>
2023-08-25 10:51:28 +0100

    chore: copy across deposit-borrow strategy into aave-like

---
+++ packages/dma-library/src/strategies/aave-like/borrow/deposit-borrow/index.ts
@@ -0,0 +1,8 @@
+export { depositBorrow } from './deposit-borrow'
+export {
+  AaveDepositBorrowArgs,
+  AaveV2DepositBorrow,
+  AaveV2DepositBorrowDependencies,
+  AaveV3DepositBorrow,
+  AaveV3DepositBorrowDependencies,
+} from './types'
