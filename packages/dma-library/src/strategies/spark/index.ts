324b839696dff32e5f425ead0cb7cd16c636fb7a
James Tuckett <james@tuckett.me>
2023-08-29 12:42:41 +0100

    feat: add payback-withdraw strategy into aave/spark

--- packages/dma-library/src/strategies/spark/index.ts
+++ packages/dma-library/src/strategies/spark/index.ts
@@ -3,11 +3,17 @@ import {
   openDepositBorrow as sparkOpenDepositBorrow,
   SparkOpenDepositBorrow,
 } from './borrow/open-deposit-borrow'
+import {
+  paybackWithdraw as sparkPaybackWithdraw,
+  SparkPaybackWithdraw,
+} from './borrow/payback-withdraw'
 
 export const spark: {
   depositBorrow: SparkDepositBorrow
   openDepositBorrow: SparkOpenDepositBorrow
+  paybackWithdraw: SparkPaybackWithdraw
 } = {
   depositBorrow: sparkDepositBorrow,
   openDepositBorrow: sparkOpenDepositBorrow,
+  paybackWithdraw: sparkPaybackWithdraw,
 }
