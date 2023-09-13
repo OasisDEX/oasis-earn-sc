5be61dfa5997a3fa73a9f643352181b41dfa0acc
Roberto Cano <3525807+robercano@users.noreply.github.com>
2023-09-13 09:03:46 +0200

    fix: github actions + lerna parallel compilation

---
+++ packages/hardhat-compiler/contracts/Dummy_0.4.24.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-or-latercompile
+pragma solidity 0.4.24;
+
+contract Dummy_0_4_24 {
+    function dummy() public pure returns (string memory) {
+        return "0.4.24";
+    }
+}
---
+++ packages/hardhat-compiler/hardhat.config.ts
@@ -0,0 +1,59 @@
+const config = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.4.21',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 0,
+          },
+        },
+      },
+      {
+        version: '0.4.24',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 0,
+          },
+        },
+      },
+      {
+        version: '0.5.17',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 0,
+          },
+        },
+      },
+      {
+        version: '0.8.18',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 0,
+          },
+        },
+      },
+      {
+        version: '0.8.15',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 0,
+          },
+        },
+      },
+    ],
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 0,
+      },
+    },
+  },
+}
+
+export default config
