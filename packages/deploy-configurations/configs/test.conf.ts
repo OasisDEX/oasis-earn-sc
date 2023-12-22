ca285748a0321cdbb7acf32bcc3ee939f95f13cd
Jakub Swierczek <jakub.swierczek@outlook.com>
2023-12-22 14:54:10 +0100

    usdc vs bridged usdc (#561)
    * Add USDC.E token to deploy configurations
    
    * v0.1.27
    
    * Update Node version and add package manager
    
    * v0.5.36
    
    * fix linter

--- packages/deploy-configurations/configs/test.conf.ts
+++ packages/deploy-configurations/configs/test.conf.ts
@@ -629,6 +629,10 @@ export const config: SystemConfig = {
       address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
       serviceRegistryName: SERVICE_REGISTRY_NAMES.common.USDC,
     },
+    'USDC.E': {
+      name: 'USDC.E',
+      address: '0x0000000000000000000000000000000000000000',
+    },
     USDBC: {
       name: 'USDBC',
       address: '0x0000000000000000000000000000000000000000',
