d99c5e8f7435c56b6c8aa674b5d7388f04edecf4
piekczyk <piekarczyk.sebastian@gmail.com>
2024-02-02 12:12:17 +0100

    Ajna dust limit validation adjustment (#610)

--- packages/dma-library/src/strategies/ajna/validation/multiply/dustLimit.ts
+++ packages/dma-library/src/strategies/ajna/validation/multiply/dustLimit.ts
@@ -1,14 +1,11 @@
-import { formatCryptoBalance } from '@dma-common/utils/common/formaters'
+import { shouldDisplayAjnaDustLimitValidation } from '@dma-library/protocols/ajna'
 import { AjnaError, AjnaPosition } from '@dma-library/types/ajna'
 
 export function validateDustLimitMultiply(position: AjnaPosition): AjnaError[] {
-  if (position.debtAmount.lt(position.pool.poolMinDebtAmount) && position.debtAmount.gt(0)) {
+  if (shouldDisplayAjnaDustLimitValidation(position)) {
     return [
       {
         name: 'debt-less-then-dust-limit-multiply',
-        data: {
-          minDebtAmount: formatCryptoBalance(position.pool.poolMinDebtAmount),
-        },
       },
     ]
   } else {
