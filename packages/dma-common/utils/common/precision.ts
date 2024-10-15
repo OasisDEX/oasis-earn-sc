95d600eb94b89c020024582b535700d5a42ffcfa
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-10-15 16:24:43 +0200

    Added docs

--- packages/dma-common/utils/common/precision.ts
+++ packages/dma-common/utils/common/precision.ts
@@ -1,10 +1,20 @@
 import BigNumber from 'bignumber.js'
 
+/**
+ * Convert base unit to fractional unit with optional precision
+ * @example
+ * amountToWei(1, 18)
+ */
 export function amountToWei(amount: BigNumber.Value, precision = 18) {
   BigNumber.config({ EXPONENTIAL_AT: 30 })
   return new BigNumber(amount || 0).times(new BigNumber(10).pow(precision))
 }
 
+/**
+ * Convert fractional unit to base unit with optional precision
+ * @example
+ * amountFromWei(1, 18)
+ */
 export function amountFromWei(amount: BigNumber.Value, precision = 18) {
   return new BigNumber(amount || 0).div(new BigNumber(10).pow(precision))
 }
