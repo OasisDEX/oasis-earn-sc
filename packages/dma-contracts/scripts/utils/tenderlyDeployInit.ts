9f8f879d0fcb3aee5f61a56d6d8367eb2f6c8d21
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-11-08 09:52:49 +0100

    Update Tenderly deployment initialization with new balance values

--- packages/dma-contracts/scripts/utils/tenderlyDeployInit.ts
+++ packages/dma-contracts/scripts/utils/tenderlyDeployInit.ts
@@ -5,13 +5,13 @@ export const tenderlyDeployInit = async (provider, opts?: { addresses: string[] 
   try {
     const txBalance = await provider.send('tenderly_setBalance', [
       [process.env.TENDERLY_FORK_ACCOUNT, extraAddresses],
-      '0xDE0B6B3A7640000',
+      '0x648030338938398870',
     ])
     console.log('sent txBalance', txBalance)
     const txErc20 = await provider.send('tenderly_setErc20Balance', [
       '0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0',
       process.env.TENDERLY_FORK_ACCOUNT,
-      '0xDE0B6B3A7640000',
+      '0x648030338938398870',
     ])
     console.log('sent txErc20', txErc20)
     // const txSnapshot = await provider.send('evm_snapshot', [])
