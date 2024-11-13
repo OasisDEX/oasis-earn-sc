e24ad1dca5b43624b2c57c7eea6eadec935ec0e5
halaprix <panicz.klos@gmail.com>
2024-11-13 10:22:40 +0100

    fix: fl for spark dai fl

--- packages/dma-library/src/strategies/aave-like/multiply/adjust/build-operation.ts
+++ packages/dma-library/src/strategies/aave-like/multiply/adjust/build-operation.ts
@@ -147,7 +147,7 @@ export async function buildAdjustFlashloan(
     collateralToken: args.collateralToken.symbol,
   })
   // We dont use the  if Spark condition since on L2s non Maker FLS are used for multiply operations
-  if (flashloanProvider !== FlashloanProvider.DssFlash) {
+  if (dependencies.protocolType === 'Spark' || flashloanProvider !== FlashloanProvider.DssFlash) {
     // Need to add fees to the swap amount
     const fromSwapAmountBeforeFees = swap.fromTokenAmount.plus(preSwapFee)
     const receivedAmountAfterSwap = swap.minToTokenAmount
