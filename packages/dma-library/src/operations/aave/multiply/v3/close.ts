a0ab2ce7c252a659dd9bcd341c502861cd2c4051
James Tuckett <james@tuckett.me>
2025-08-15 11:43:47 +0100

    fix: adjust flashloan withdrawal amount for Aave V3 compatibility

--- packages/dma-library/src/operations/aave/multiply/v3/close.ts
+++ packages/dma-library/src/operations/aave/multiply/v3/close.ts
@@ -46,6 +46,7 @@ export const close: AaveV3CloseOperation = async ({
   addresses,
   network,
 }) => {
+  console.log('[DEBUG] AaveV3 Close: FL Amount', flashloan.token.amount)
   const setEModeOnCollateral = actions.aave.v3.aaveV3SetEMode(network, {
     categoryId: 0,
   })
@@ -102,9 +103,15 @@ export const close: AaveV3CloseOperation = async ({
     onBehalfOf: proxy.address,
   })
 
+  // Aave V3 changed how they round numbers in one of their contracts
+  // They effect is that we can no longer immediately withdraw the full amount of the flashloan
+  // This is a workaround to ensure we can repay the flashloan in full
+  // https://github.com/aave-dao/aave-v3-origin/commit/070cd23d949d828ad78d098ef481c2049f5eabb6#diff-b7654541d4083b2f7a8e06e4359f5082eca09c6d890e7b5af914395e15fb04b5
+  const withdrawFlashLoanAmount = flashloan.token.amount.minus(1)
+  console.log('[DEBUG] AaveV3 Close: Withdrawn FL Amount', withdrawFlashLoanAmount)
   const withdrawFlashLoan = actions.aave.v3.aaveV3Withdraw(network, {
     asset: flashloan.token.address,
-    amount: flashloan.token.amount,
+    amount: withdrawFlashLoanAmount,
     to: addresses.operationExecutor,
   })
 
